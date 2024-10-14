1013761f907433d3b1e3bc3d6194f6b6153164fe
Roby Aryanata <robyaryanata12345@gmail.com>
2024-10-14 07:11:22 +0700

    port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express, { query } from 'express';
+import express from 'express';
 import connectDB from './utils/mongodb.js';
 import cors from 'cors';
 import { postUser, getUser } from './controller/userController.js';
@@ -8,16 +8,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/auth/register', postUser);
-
-app.get('/auth/:userId', getUser)
+app.get('/auth/:userId', getUser);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
